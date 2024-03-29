05fcbb4ec2245a47c126bcfa03dffaa77ef1e543
Guilherme Fragnani <fragnaniguilherme@gmail.com>
2024-03-29 15:04:50 -0300

    Update ex1Nelio.java

--- ex1Nelio.java
+++ ex1Nelio.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
+
 public class ex1Nelio {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-  int n1;
-  System.out.println("Digite um numero: ");
-  n1 = sc.nextInt();
-  
-  if (n1 >= 0){
-    System.out.println("Esse número é positivo!");
-    }else{
+    int n1;
+    System.out.println("Digite um numero: ");
+    n1 = sc.nextInt();
+
+    if (n1 >= 0) {
+      System.out.println("Esse número é positivo!");
+    } else {
       System.out.println("Esse número é negativo!");
       sc.close();
+      // comentario
     }
   }
 }
